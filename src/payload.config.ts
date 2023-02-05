2c629e6acf510cf43c20bcc452986545c4445934
jussmor <jussmor@outlook.es>
2023-02-05 17:15:23 -0500

    fix port

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,7 +6,7 @@ import Tags from './collections/Tags';
 import Users from './collections/Users';
 
 export default buildConfig({
-  serverURL: 'http://localhost:3000',
+  serverURL: 'https://jussmorapi-production.up.railway.app',
   admin: {
     user: Users.slug,
   },
